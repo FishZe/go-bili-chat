b2fcb8e7157fce3eaba582ac83bdc75d312bbbf5
yawntee <yawntee@qq.com>
2023-05-11 20:07:05 +0800

    送礼部分新增盲盒礼物结构与主播信息结构

--- handler/template.go
+++ handler/template.go
@@ -429,63 +429,79 @@ type RoomRealTimeMessageUpdate struct {
 	} `json:"data"`
 }
 
+type BlindGift struct {
+	BlindGiftConfigId int    `json:"blind_gift_config_id"`
+	From              int    `json:"from"`
+	GiftAction        string `json:"gift_action"`
+	GiftTipPrice      int    `json:"gift_tip_price"`
+	OriginalGiftId    int    `json:"original_gift_id"`
+	OriginalGiftName  string `json:"original_gift_name"`
+	OriginalGiftPrice int    `json:"original_gift_price"`
+}
+
+type ReceiveUserInfo struct {
+	Uid   int    `json:"uid"`
+	Uname string `json:"uname"`
+}
+
 type SendGift struct {
 	Cmd  string `json:"cmd"`
 	Data struct {
-		UID               int64       `json:"uid"`
-		Name              string      `json:"uname"`
-		NameColor         string      `json:"name_color"`
-		Action            string      `json:"action"`
-		BatchComboID      string      `json:"batch_combo_id"`
-		BatchComboSend    interface{} `json:"batch_combo_send"`
-		BeatID            string      `json:"beatId"`
-		BizSource         string      `json:"biz_source"`
-		BlindGift         interface{} `json:"blind_gift"`
-		BroadcastID       int         `json:"broadcast_id"`
-		CoinType          string      `json:"coin_type"`
-		ComboResourcesID  int         `json:"combo_resources_id"`
-		ComboSend         interface{} `json:"combo_send"`
-		ComboStayTime     int         `json:"combo_stay_time"`
-		ComboTotalCoin    int         `json:"combo_total_coin"`
-		CritProb          int         `json:"crit_prob"`
-		Demarcation       int         `json:"demarcation"`
-		DiscountPrice     int         `json:"discount_price"`
-		Dmscore           int         `json:"dmscore"`
-		Draw              int         `json:"draw"`
-		Effect            int         `json:"effect"`
-		EffectBlock       int         `json:"effect_block"`
-		Face              string      `json:"face"`
-		FaceEffectID      int         `json:"face_effect_id"`
-		FaceEffectType    int         `json:"face_effect_type"`
-		FloatScResourceID int         `json:"float_sc_resource_id"`
-		GiftID            int         `json:"giftId"`
-		GiftName          string      `json:"giftName"`
-		GiftType          int         `json:"giftType"`
-		Gold              int         `json:"gold"`
-		GuardLevel        int         `json:"guard_level"`
-		IsFirst           bool        `json:"is_first"`
-		IsNaming          bool        `json:"is_naming"`
-		IsSpecialBatch    int         `json:"is_special_batch"`
-		Magnification     float64     `json:"magnification"`
-		Num               int         `json:"num"`
-		OriginalGiftName  string      `json:"original_gift_name"`
-		Price             int         `json:"price"`
-		Rcost             int         `json:"rcost"`
-		Remain            int         `json:"remain"`
-		Rnd               string      `json:"rnd"`
-		SendMaster        interface{} `json:"send_master"`
-		Silver            int         `json:"silver"`
-		Super             int         `json:"super"`
-		SuperBatchGiftNum int         `json:"super_batch_gift_num"`
-		SuperGiftNum      int         `json:"super_gift_num"`
-		SvgaBlock         int         `json:"svga_block"`
-		Switch            bool        `json:"switch"`
-		TagImage          string      `json:"tag_image"`
-		Tid               string      `json:"tid"`
-		Timestamp         int         `json:"timestamp"`
-		TopList           interface{} `json:"top_list"`
-		TotalCoin         int         `json:"total_coin"`
-		FansMedal         FansMedal   `json:"medal_info"`
+		UID               int64           `json:"uid"`
+		Name              string          `json:"uname"`
+		NameColor         string          `json:"name_color"`
+		Action            string          `json:"action"`
+		BatchComboID      string          `json:"batch_combo_id"`
+		BatchComboSend    interface{}     `json:"batch_combo_send"`
+		BeatID            string          `json:"beatId"`
+		BizSource         string          `json:"biz_source"`
+		BlindGift         *BlindGift      `json:"blind_gift"`
+		BroadcastID       int             `json:"broadcast_id"`
+		CoinType          string          `json:"coin_type"`
+		ComboResourcesID  int             `json:"combo_resources_id"`
+		ComboSend         interface{}     `json:"combo_send"`
+		ComboStayTime     int             `json:"combo_stay_time"`
+		ComboTotalCoin    int             `json:"combo_total_coin"`
+		CritProb          int             `json:"crit_prob"`
+		Demarcation       int             `json:"demarcation"`
+		DiscountPrice     int             `json:"discount_price"`
+		Dmscore           int             `json:"dmscore"`
+		Draw              int             `json:"draw"`
+		Effect            int             `json:"effect"`
+		EffectBlock       int             `json:"effect_block"`
+		Face              string          `json:"face"`
+		FaceEffectID      int             `json:"face_effect_id"`
+		FaceEffectType    int             `json:"face_effect_type"`
+		FloatScResourceID int             `json:"float_sc_resource_id"`
+		GiftID            int             `json:"giftId"`
+		GiftName          string          `json:"giftName"`
+		GiftType          int             `json:"giftType"`
+		Gold              int             `json:"gold"`
+		GuardLevel        int             `json:"guard_level"`
+		IsFirst           bool            `json:"is_first"`
+		IsNaming          bool            `json:"is_naming"`
+		IsSpecialBatch    int             `json:"is_special_batch"`
+		Magnification     float64         `json:"magnification"`
+		Num               int             `json:"num"`
+		OriginalGiftName  string          `json:"original_gift_name"`
+		Price             int             `json:"price"`
+		Rcost             int             `json:"rcost"`
+		ReceiveUserInfo   ReceiveUserInfo `json:"receive_user_info"`
+		Remain            int             `json:"remain"`
+		Rnd               string          `json:"rnd"`
+		SendMaster        interface{}     `json:"send_master"`
+		Silver            int             `json:"silver"`
+		Super             int             `json:"super"`
+		SuperBatchGiftNum int             `json:"super_batch_gift_num"`
+		SuperGiftNum      int             `json:"super_gift_num"`
+		SvgaBlock         int             `json:"svga_block"`
+		Switch            bool            `json:"switch"`
+		TagImage          string          `json:"tag_image"`
+		Tid               string          `json:"tid"`
+		Timestamp         int             `json:"timestamp"`
+		TopList           interface{}     `json:"top_list"`
+		TotalCoin         int             `json:"total_coin"`
+		FansMedal         FansMedal       `json:"medal_info"`
 	} `json:"data"`
 }
 
